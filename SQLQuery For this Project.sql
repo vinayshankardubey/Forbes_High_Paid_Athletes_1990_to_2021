12f2f39adbd7ec81e516755d1c7986f5ec242fea
VinayDevs00 <vinaydevs00@gmail.com>
2021-12-29 15:01:49 +0530

    today

--- SQLQuery For this Project.sql
+++ SQLQuery For this Project.sql
@@ -5,11 +5,18 @@ use forbesTopAthletes
 #Uploading Dataset and use as Table
 
 
-SELECT * FROM forbesathletesv2 
+SELECT * FROM forbesathletesv2 ORDER BY auto_id DESC
 
 
 ALTER TABLE forbesathletesv2 ADD auto_id int IDENTITY(1,1)
 
 
 SELECT *
-FROM forbesathletesv2 
+FROM (SELECT *,RANK() OVER (Partition By Year Order BY auto_id DESC) AS r FROM forbesathletesv2) as f
+Where f.r <= 10
+
+
+SELECt * INTO topforbes_athletes FROM (SELECT *,RANK() OVER (Partition By Year Order BY auto_id DESC) AS r FROM forbesathletesv2) as f
+Where f.r <= 10
+
+SELECT * FROM topforbes_athletes
